5e1964a4a8427c087cb70b6ed322a29497f42260
mpfthprblmtq <ripley307@gmail.com>
2023-10-27 21:49:23 -0500

    Added redux, now page is consistent when you refresh

--- src/components/PartCollector/PartCollectorComponent.tsx
+++ src/components/PartCollector/PartCollectorComponent.tsx
@@ -1,22 +1,27 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent } from "react";
 import NavBar from "../_shared/NavBar/NavBar";
 import Version from "../_shared/Version/Version";
 import { Tabs } from "../_shared/NavBar/Tabs";
 import { CurrentView } from "./CurrentView";
 import AddPartsComponent from "./AddParts/AddPartsComponent";
 import ViewPartsComponent from "./ViewParts/ViewPartsComponent";
+import { useDispatch, useSelector } from "react-redux";
+import { updateCurrentView } from "../../redux/slices/partSlice";
 
 const PartCollectorComponent: FunctionComponent = () => {
 
-  const [currentView, setCurrentView] = useState<CurrentView>(CurrentView.ADD_PARTS);
+  // const [currentView, setCurrentView] = useState<CurrentView>(CurrentView.ADD_PARTS);
+
+  const dispatch = useDispatch();
+  const currentView: CurrentView = useSelector((state: any) => state.partStore.currentView);
 
   return (
     <div className={"App"}>
       <NavBar
         activeTab={Tabs.PART_COLLECTOR}
         currentView={currentView}
-        showAddParts={() => setCurrentView(CurrentView.ADD_PARTS)}
-        showViewParts={() => setCurrentView(CurrentView.VIEW_PARTS)} />
+      showAddParts={() => dispatch(updateCurrentView(CurrentView.ADD_PARTS))}
+      showViewParts={() => dispatch(updateCurrentView(CurrentView.VIEW_PARTS))} />
       <Version />
       {currentView === CurrentView.ADD_PARTS && (
         <AddPartsComponent />
