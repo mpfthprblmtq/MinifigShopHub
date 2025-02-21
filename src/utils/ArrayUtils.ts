ad44dd9288d6d06c3e5d9a5ef29a4a89b92b7ab3
mpfthprblmtq <pat@prblmtq.com>
2025-02-20 22:29:11 -0600

    Bugfix where on bulk loading items with the same id, it makes it impossible to edit those same id items

--- src/utils/ArrayUtils.ts
+++ src/utils/ArrayUtils.ts
@@ -1,4 +1,5 @@
 import {Item} from "../model/item/Item";
+import _ from "lodash";
 
 export const generateId = (items: Item[]): number => {
     if (items.length === 0) {
@@ -11,6 +12,8 @@ export const getItemWithId = (items: Item[], id: number): Item | undefined => {
     return items.filter(item => item.id === id)[0];
 }
 
-export const retainOriginalOrder = (originalIds: string[], resultMap: Map<string, any[]>) => {
-    return originalIds.map(id => ({ id, results: resultMap.get(id) || [] }));
+export const retainOriginalOrder = (originalIds: string[], resultMap: Map<string, any[]>): {id: string, results: Item[]}[] => {
+    // we have to use clonedeep here because this will map the same object in memory to the same id, making it
+    // impossible to make updates to a single item in the list
+    return originalIds.map(id => ({ id, results: _.cloneDeep(resultMap.get(id)) || [] }));
 };
